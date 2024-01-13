b1d9d3721fb8403453631db9ae2b1947481e1f01
Chetankamde30 <Kamdeche3@gmail.com>
2024-01-13 15:26:17 +0530

    first commit

---
+++ DemoRegAndLoginWithIdentity/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using DemoRegAndLoginWithIdentity.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoRegAndLoginWithIdentity.Controllers
+{
+
+    [Authorize(Roles="Admin")]
+    public class AdminController : Controller
+    {
+
+      
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+       
+        public AdminController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddService()
+        {
+            return View();
+        }
+        public IActionResult GetBooking()
+        {
+            return View();
+        }
+        public IActionResult GetContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleStore rolestore)
+        {
+            var alreadyAdded = await _roleManager.RoleExistsAsync(rolestore.RoleName);
+
+            if (!alreadyAdded)
+            {
+                await _roleManager.CreateAsync(new IdentityRole(rolestore.RoleName));
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ DemoRegAndLoginWithIdentity/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DemoRegAndLoginWithIdentity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DemoRegAndLoginWithIdentity.DTO;
+
+namespace DemoRegAndLoginWithIdentity.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<DemoRegAndLoginWithIdentity.DTO.RoleStore>? RoleStore { get; set; }
+    }
+}
